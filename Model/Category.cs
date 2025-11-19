fdd0b00c3ce02bc9026ac789a9fde9f4420ce485
hani manaa <hanimanaa@gmail.com>
2025-11-19 20:05:40 +0200

    add model folder

---
+++ Model/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Category
+    {
+        public int catNum { get; set; }
+        public string catName { get; set; }
+    }
+}
---
+++ Model/ProductList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Model
+{
+    [CollectionDataContract]
+    public class ProductList : List<Product>
+    {
+        public ProductList() { }
+
+        public ProductList(IEnumerable<Product> list) : base(list) { }
+    }
+}
