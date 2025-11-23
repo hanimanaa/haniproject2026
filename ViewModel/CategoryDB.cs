c0ba65d8864de8b072e1ef84b0cf19885a2df239
hani manaa <hanimanaa@gmail.com>
2025-11-23 18:06:13 +0200

    add view model

---
+++ ViewModel/CategoryDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ViewModel
+{
+    public class CategoryDB : DBFunctuins
+    {
+        Category category = null;
+        CategoryList categoriesList = new CategoryList();
+
+        public CategoryDB() : base() { }
+
+        private Category CreateModel(Category category)
+        {
+            category.catNum = int.Parse(reader["catNum"].ToString());
+            category.catName =reader["catName"].ToString();
+            return category;
+        }
+
+       
+    }
+}
---
+++ ViewModel/ProductDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ViewModel
+{
+    public class ProductDB : DBFunctuins
+    {
+        Product product = null;
+        ProductList ProductsList = new ProductList();
+
+        public ProductDB() : base() { }
+
+        private Product CreateModel(Product product)
+        {
+            product.productNum = int.Parse(reader["productNum"].ToString());
+            product.productName = reader["productName"].ToString();
+            product.price = double.Parse(reader["price"].ToString());
+            product.imageUrl = reader["imageUrl"].ToString();
+            product.description = reader["description"].ToString();
+
+            int catNum = int.Parse(reader["catNum"].ToString());
+            CategoryDB categoryDB = new CategoryDB();
+            product.category = categoryDB.SelectCategoryByNum(catNum);
+
+            product.expiredDate = DateTime.Parse(reader["expiredDate"].ToString());
+            product.vegan = (reader["imageUrl"].ToString());
+
+
+
+
+
+            return product;
+        }
+    }
+}
