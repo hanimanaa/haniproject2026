da4ef1f3dee831ba9f4c4e0e5b685b6bc12002b5
hanimanaa <hanimanaa@gmail.com>
2025-11-24 15:23:20 +0200

    update view model files

--- ViewModel/CategoryDB.cs
+++ ViewModel/CategoryDB.cs
@@ -20,7 +20,31 @@ namespace ViewModel
             category.catName =reader["catName"].ToString();
             return category;
         }
+        public int AddCategory(Category category)
+        {
+            string insertSql = string.Format("Insert into CategoryTbl "
+                + "(catNum,catName)"
+                + " values ({0},'{1}')"
+                , category.catNum, category.catName);
+            return base.ChangeTable(insertSql, "Database2026.accdb");
 
-       
+        }
+        // Delete Category By CatNum
+        public int DeleteCategoryByCatNum(int catNum)
+        {
+            string delSql = string.Format("Delete from CategoryTbl "
+                + "where catNum= {0}", catNum);
+            return base.ChangeTable(delSql, "Database2026.accdb");
+        }
+        // Update Category 
+        public int UpdateCategory(Category category)
+        {
+            string updateSql = string.Format("Update CategoryTbl SET "
+                + "catName='{0}'"  
+                + " where catNum={1}", category.catName, category.catNum);
+            return base.ChangeTable(updateSql, "MyDatabase.accdb");
+        }
+
+
     }
 }
--- ViewModel/ProductDB.cs
+++ ViewModel/ProductDB.cs
@@ -27,13 +27,42 @@ namespace ViewModel
             product.category = categoryDB.SelectCategoryByNum(catNum);
 
             product.expiredDate = DateTime.Parse(reader["expiredDate"].ToString());
-            product.vegan = (reader["imageUrl"].ToString());
-
-
-
-
+            product.vegan = (reader["vegan"].ToString());
 
             return product;
+        }
+
+        public int AddProduct(Product product)
+        {
+            string insertSql = string.Format("Insert into ProductTbl "
+                + "(productNum,productName,price,imageUrl,description,catNum,expiredDate,vegan)"
+                + " values ({0},'{1}',{2},'{3}','{4}',{5},{6},{7})"
+                ,product.productNum,product.productName,product.price,product.imageUrl,
+                product.description,product.category.catNum,product.expiredDate,product.vegan);
+
+            return base.ChangeTable(insertSql, "Database2026.accdb");
+
+        }
+        // Update product
+        public int UpdateProduct(Product product)
+        {
+            string updateSql = string.Format("Update ProductTbl SET "
+                + "productName={0},price={1},imageUrl={2},description={3}"
+                + ",catNum={4},expiredDate={5},vegan={6}"
+                + " where productNum={7}"
+                ,product.productName, product.price, product.imageUrl,
+                product.description, product.category.catNum, product.expiredDate,
+                product.vegan, product.productNum);
+
+            return base.ChangeTable(updateSql, "Database2026.accdb");
+        }
+
+        // Delete Product By ProductNum
+        public int DeleteProductByProductNum(int productNum)
+        {
+            string delSql = string.Format("Delete from ProductTbl "
+                + "where productNum= {0}", productNum) ;
+            return base.ChangeTable(delSql, "Database2026.accdb");
         }
     }
 }
