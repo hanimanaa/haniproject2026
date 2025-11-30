30881c9b20b770557a901fb0665e9f05612855ff
hanimanaa <hanimanaa@gmail.com>
2025-11-30 16:06:45 +0200

    add wcf

---
+++ Wcf/Service1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Model;
+using ViewModel;
+
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        CategoryDB categoryDB = new CategoryDB();
+
+
+        // CategoryDB
+        public CategoryList SelectAllCategories()
+        {
+            return categoryDB.SelectAllCategories();
+        }
+        public Category SelectCategoryByNum(int num)
+        {
+            return categoryDB.SelectCategoryByNum(num);
+        }
+
+
+
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
